4c2a74e2b724cba02947db6c6cdcbf4ee17e4c18
Halle-Astra <halle.wang@qq.com>
2019-12-05 11:27:17 +0800

    Fix it for py3

--- ipynb2pdf.py
+++ ipynb2pdf.py
@@ -1,4 +1,4 @@
-'''Got this file from https://www.jianshu.com/p/49a0c9f74d59'''
+'''Got this file from https://www.jianshu.com/p/49a0c9f74d59 and made it be suit for python3'''
 import sys
 import subprocess
 import pdfkit
@@ -6,7 +6,7 @@ inputfile = sys.argv[1].replace(" ","\ ")
 temp_html = inputfile[0:inputfile.rfind('.')]+'.html'
 command = 'ipython nbconvert --to html ' + inputfile
 subprocess.call(command,shell=True)
-print '============success==========='
+print('============success===========')
 output_file =inputfile[0:inputfile.rfind('.')]+'.pdf'
 pdfkit.from_file(temp_html,output_file)
 subprocess.call('rm '+temp_html,shell=True)
